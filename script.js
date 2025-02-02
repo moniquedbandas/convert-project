33ede16bef33445fd013c4c5e59dbae90483c6e5
MoniqueDB <moniquedbandas@gmail.com>
2025-02-01 22:26:55 -0300

    Projeto de conversão de valores de moedas finalizado

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const form = document.querySelector("form");
 const amount = document.getElementById("amount");
 const currency = document.getElementById("currency");
 const footer = document.querySelector("main footer");
+const description = document.getElementById("description");
+const result = document.getElementById("result");
 //manipulando o input amount para receber somente números
 //quando entrar conteúdo no input, vai executar a função
 
@@ -34,6 +36,24 @@ form.onsubmit = (event) => {
 
 function convertCurrency(amount, price, symbol) {
   try {
+    //exibinddo a cotação da moeda selecionada
+    description.textContent = `${symbol} 1 = ${formatCurrencyBRL(price)}`;
+
+    //calcula o total
+    // let total = String(amount * price).replace(".", ","); uma outra forma de formatar o valor
+
+    let total = amount * price;
+    //verifica se o resultado nao é um numero
+    if (isNaN(total)) {
+      return alert("Por favor, digite um valor correto");
+    }
+    //formatar o valor total
+    total = formatCurrencyBRL(total).replace("RS", "");
+    // ou
+
+    //exibir o resultado total
+    result.textContent = `${total} Reais`;
+
     //aplica a classe que exibe o footer para mostrar o resultado
     footer.classList.add("show-result");
   } catch (error) {
@@ -42,4 +62,13 @@ function convertCurrency(amount, price, symbol) {
     console.log(error);
     alert("Não foi possível converter. Tente novamente mais tarde.");
   }
+}
+
+//formata a moeda em real brasileiro
+function formatCurrencyBRL(value) {
+  //primeiro converte para numero para utilizar o tolocalestring para entao formatar no padrao BRL
+  return Number(value).toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
 }
